78ba6a5990e1de4fb38b5ee51b9174ed3b40229b
Sasha Petrenko <sap625@mst.edu>
2022-05-31 15:02:51 -0500

    Add pl10k install

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -28,6 +28,12 @@ try_install () {
     fi
 }
 
+# Zsh Powerlevel10k
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+LOCAL_DIR=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+LOCAL_COMMAND="git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $LOCAL_DIR"
+try_install $LOCAL_DIR $LOCAL_COMMAND
+
 # Zsh syntax highlighting
 # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 LOCAL_DIR=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting

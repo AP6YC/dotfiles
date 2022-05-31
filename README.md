05bd9c2eac83f12d726474fa09d2850351ae6ec0
Sasha Petrenko <sap625@mst.edu>
2022-05-31 14:24:19 -0500

    Add references and startup script

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dotfiles
 Personal dotfiles for terminal development and deployment.
+
+## References
+
+- Tmux theme: https://github.com/gpakosz/.tmux
+- Oh My Zsh: https://github.com/ohmyzsh/ohmyzsh
+    - Zsh autosuggestions: https://github.com/zsh-users/zsh-autosuggestions
+    - Zsh syntax highlighting: https://github.com/zsh-users/zsh-syntax-highlighting
--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
 # sudo apt install ripgrep
+
+# Zsh syntax highlighting
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+LOCAL_DIR=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+[ ! -d $LOCAL_DIR ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $LOCAL_DIR || echo "Zsh syntax highlighting already installed"
+
+# Zsh autosuggestions
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+LOCAL_DIR=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+[ ! -d $LOCAL_DIR ] && git clone https://github.com/zsh-users/zsh-autosuggestions $LOCAL_DIR || echo "Zsh autosuggestions already installed"

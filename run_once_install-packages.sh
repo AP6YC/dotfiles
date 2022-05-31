d645f5e517ef8a7be6b86a29b7480a17c1c32a15
Sasha Petrenko <sap625@mst.edu>
2022-05-31 14:46:10 -0500

    Clean up install script

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,12 +1,41 @@
 #!/bin/sh
 # sudo apt install ripgrep
 
+"""
+    already_installed
+
+Echos that a directory has already been installed.
+
+* `$1` - Directory that is already installed.
+"""
+already_installed () {
+    echo "------- SASHA'S CM - Already installed: $1"
+}
+
+"""
+    try_install
+
+Tries to run the command if the target directory does not exist.
+* `$1` - Directory to check.
+* `$2` - String, command to be executed.
+"""
+try_install () {
+    if [ ! -d $1 ]
+    then
+        eval $2
+    else
+        already_installed $1
+    fi
+}
+
 # Zsh syntax highlighting
 # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 LOCAL_DIR=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-[ ! -d $LOCAL_DIR ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $LOCAL_DIR || echo "Zsh syntax highlighting already installed"
+LOCAL_COMMAND="git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $LOCAL_DIR"
+try_install $LOCAL_DIR $LOCAL_COMMAND
 
 # Zsh autosuggestions
 # git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 LOCAL_DIR=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-[ ! -d $LOCAL_DIR ] && git clone https://github.com/zsh-users/zsh-autosuggestions $LOCAL_DIR || echo "Zsh autosuggestions already installed"
+LOCAL_COMMAND="git clone https://github.com/zsh-users/zsh-autosuggestions $LOCAL_DIR"
+try_install $LOCAL_DIR $LOCAL_COMMAND
